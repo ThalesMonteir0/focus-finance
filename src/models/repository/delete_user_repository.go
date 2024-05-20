b2dbdebbc64a00722506bfbdd885d5f8dc8628f2
ThalesMonteir0 <thales.monteiro.moreira@gmail.com>
2024-05-20 07:37:12 -0300

    refactor: refatorando os erros do user repository

--- src/models/repository/delete_user_repository.go
+++ src/models/repository/delete_user_repository.go
@@ -1,23 +1,17 @@
 package repository
 
 import (
-	"errors"
 	"focus-finance/src/configuration/rest_err"
 	"focus-finance/src/models/repository/entity"
-	"gorm.io/gorm"
 )
 
 func (ur *userRepository) DeleteUserRepository(id int) *rest_err.RestErr {
 	var user entity.User
 
 	err := ur.DB.Delete(&user, id).Error
-	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+	if err != nil {
+		//todo:logs
 		err := rest_err.NewInternalServerError("unable delete user")
-		return err
-	}
-
-	if errors.Is(err, gorm.ErrRecordNotFound) {
-		err := rest_err.NewBadRequestError("user not found")
 		return err
 	}
 
--- src/models/repository/find_user_repository.go
+++ src/models/repository/find_user_repository.go
@@ -1,25 +1,19 @@
 package repository
 
 import (
-	"errors"
 	"focus-finance/src/configuration/rest_err"
 	"focus-finance/src/models"
 	"focus-finance/src/models/repository/converter"
 	"focus-finance/src/models/repository/entity"
-	"gorm.io/gorm"
 )
 
 func (ur *userRepository) FindUserByID(id int) (models.UserDomainInterface, *rest_err.RestErr) {
 	var user entity.User
 
 	err := ur.DB.First(&user, id).Error
-	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+	if err != nil || user.ID == 0 {
+		//todo:logs
 		err := rest_err.NewInternalServerError("unable get user")
-		return nil, err
-	}
-
-	if user.ID == 0 || errors.Is(err, gorm.ErrRecordNotFound) {
-		err := rest_err.NewInternalServerError("user not found")
 		return nil, err
 	}
 
@@ -30,13 +24,8 @@ func (ur *userRepository) FindUserByEmail(email string) (models.UserDomainInterf
 	var user entity.User
 
 	err := ur.DB.Where("email = ?", email).First(&user).Error
-	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+	if err != nil || user.ID == 0 {
 		err := rest_err.NewInternalServerError("unable get user")
-		return nil, err
-	}
-
-	if user.ID == 0 || errors.Is(err, gorm.ErrRecordNotFound) {
-		err := rest_err.NewBadRequestError("user not found")
 		return nil, err
 	}
 
